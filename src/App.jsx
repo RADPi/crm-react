f1e1ecdc41c996193a0c45a13da3b00a4c70032b
Rafael Davila Pico <radp71@gmail.com>
2022-01-15 10:32:02 -0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import VerCliente from './components/VerCliente'
-import IniciarSesion from './layout/IniciarSesion'
 import Layout from './layout/Layout'
 import EditarCliente from './pages/EditarCliente'
 import Inicio from './pages/Inicio'
-import LoginForm from './pages/LoginForm'
 import NuevoCliente from './pages/NuevoCliente'
 
 function App() {
@@ -16,9 +14,6 @@ function App() {
 			<div>
 				<BrowserRouter>
 					<Routes>
-						<Route path="/" element={<IniciarSesion />}>
-							<Route index element={<LoginForm />} />
-						</Route>
 						<Route path="/clientes" element={<Layout />}>
 							<Route index element={<Inicio />} />
 							<Route path="nuevo" element={<NuevoCliente />} />
